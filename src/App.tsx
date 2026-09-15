c97a723baea0c20a86d36258eb689223c3ef3621
utouto <utoutokurouto@gmail.com>
2026-09-15 16:22:53 +0000

    ヘッダーのアイコンを48pxに調整

--- src/App.tsx
+++ src/App.tsx
@@ -559,7 +559,7 @@ export default function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <img src="/icons/nuito-icon.png" alt="" width="64" height="64" />
+        <img src="/icons/nuito-icon.png" alt="" width="48" height="48" />
         <strong>ぬいと</strong>
       </header>
       <main className={view === "today" ? "today-main" : undefined}>
